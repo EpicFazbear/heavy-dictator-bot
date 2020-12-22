7bd56fbaff240384f8c34fbe28eadd96ca51cddb
EpicFazbear <rod2wang2wa@gmail.com>
2020-12-22 12:02:20 -0600

    Did some work
    Mostly it's just changes such as adding the isAdmin function, adding some new commands to change the internal variables, adding an error handler, etc.

--- functions.lua
+++ functions.lua
@@ -5,8 +5,17 @@ return function(ENV)
 		commands = require("./commands.lua")(ENV); -- Loads in the commands into the table so that it can get loaded into the main environment later.
 
 		sleep = function(n) -- In seconds
-		  local t0 = os.clock()
-		  while os.clock() - t0 <= n do end
+			local t0 = os.clock()
+			while os.clock() - t0 <= n do end
+		end;
+
+		isAdmin = function(userId)
+			for _, Id in pairs(admins) do
+				if userId == Id then
+					return true
+				end
+			end
+			return false
 		end;
 	};
 end;
