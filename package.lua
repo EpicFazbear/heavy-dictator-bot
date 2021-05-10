1a133209c6f9e15c3154309254fb5d1473b3d512
EpicFazbear <EpicFazbear@users.noreply.github.com>
2021-05-10 11:07:03 -0500

    Update package.lua
    Enumerated version number (New minor update with a bunch new functions)
    Made a comment about the Git submodule dependency (Heroku loads this in automatically)

--- package.lua
+++ package.lua
@@ -1,6 +1,6 @@
 return {
   name = "discord-bot-hosting",
-  version = "1.1.0",
+  version = "1.2.0",
   description = "Hosting discord bots on Heroku (Heavy Dictator)",
   main = "botmain.lua",
   scripts = {
@@ -8,5 +8,6 @@ return {
   },
   dependencies = {
     "SinisterRectus/discordia"
+--	,"JustMaximumPower/luvit-postgres" -- Git submodule
   },
 }
