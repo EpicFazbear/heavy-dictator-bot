24946ae3abd172020b9fbd679e532c65d0b8525f
EpicFazbear <rod2wang2wa@gmail.com>
2021-05-10 12:35:55 -0500

    Overhaul Work - Part 1
    .env.template - Added new ENV variables
    botmain.lua - Preliminary changes for future SQL integration
    commands.lua - Finally overhauled the format of the commands (botmain.lua still needs to be modified in order to handle this, UPCOMING SOON)
    Also added message conditions for channel commands that notifies the user if an inputted channel ID is invalid or not
    functions.lua - Preliminary SQL dependency, Removed obsoleted functions (that were commented out)

--- botmain.lua
+++ botmain.lua
@@ -1,3 +1,6 @@
+-- Remember to update version in package.lua!
+-- TODO: Debug cmd.Run(cmd, message) switched with cmd:Run(message)
+
 local discordia = require("discordia")
 local json = require("json")
 local ENV = process.env
@@ -12,37 +15,52 @@ table.insert(admins, owner)
 
 
 mainChannel = ENV.MAIN_CHANNEL
-destChannel = nil
-coalmine = nil
-coal = 0
+destChannel = ENV.DEST_OVERRIDE
+coalmine = ENV.COAL_OVERRIDE
+coal = 0 -- TODO: Add percentages (amount person worked to total) for payout
 reached = false
 paid = {}
 workers = {}
 
-minGoal = 100
-maxGoal = 300
-minPay = 750
-maxPay = 1000
-cvRate = 0.015472
+minGoal = 100 -- ENV.GOAL_MIN
+maxGoal = 300 -- ENV.GOAL_MAX
+minPay = 750 -- ENV.PAY_MIN
+maxPay = 1000 -- ENV.PAY_MAX
+cvRate = 0.015472 -- ENV.CV_RATE -- TODO: better way of doing this (ratio maybe)
 goal = math.random(minGoal, maxGoal)
 
 
+-- Injects our external functions into this main script
 local functions = require("./functions.lua")(getfenv(1))
 local previous = getfenv(1)
 for i,v in pairs(functions) do previous[i] = v end
-setfenv(1, previous) -- Loads our functions
+setfenv(1, previous)
 
 
 client:on("ready", function()
+	owner = ownerOverride or client.owner.id
+	local message = client:getChannel(mainChannel):send("***Starting bot..***")
 	if ENV.INVISIBLE == "true" then
 		client:setStatus("invisible") -- Bravo Six, going dark.
+	else
+		client:setStatus("idle")
+		client:setGame("Initializing..")
 	end
-	if string.lower(ENV.STATUS) ~= "none" then
-		client:setGame(ENV.STATUS)
+	
+	-- TODO: in here, do loading sequence with SQL stuffs
+	if sql_thingy then
+		message:setContent(message.content .. "\n***Retrieving data from SQL Database..***")
+		print("Retrieving data from SQL Database..")
 	end
-	owner = ownerOverride or client.owner.id
-	client:getChannel(mainChannel):send("***{!} Heavy dictator has been started. {!}***")
-	print("\nHeavy dictator now activating.. Gulag Mode enabled.")
+
+	if ENV.INVISIBLE ~= "true" then
+		client:setStatus("online")
+		if string.lower(ENV.STATUS) ~= "none" then
+			client:setGame(ENV.STATUS)
+		end
+	end
+	message:setContent(message.content .. "\n***{!} Heavy dictator has been started. {!}***")
+	print("Heavy dictator now activating.. Gulag Mode enabled.")
 end)
 
 
@@ -51,6 +69,7 @@ client:on("messageCreate", function(message)
 
 	if string.sub(string.lower(message.content), 1, 1) == prefix then
 		for i=1, #commands do -- Runs through our list of commands and connects them to our messageCreate connection
+			-- TODO: Finish overhaul formatting to ["NAME"] = function()
 			local cmd = commands[i]
 			if string.sub(string.lower(message.content), 1, string.len(prefix) + string.len(cmd.Name)) == string.lower(prefix .. cmd.Name) then
 				local ran, error = pcall(function()
