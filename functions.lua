5c05fefb264f4766330ab258383d25c661eff1a8
EpicFazbear <EpicFazbear@users.noreply.github.com>
2021-05-13 18:03:02 -0500

    Overhaul Merge Mission (#7)
    Authored-by: vellaG, EpicFazbear
    
    
    * Overhaul Work - Part 1
    .env.template - Added new ENV variables
    botmain.lua - Preliminary changes for future SQL integration
    commands.lua - Finally overhauled the format of the commands (botmain.lua still needs to be modified in order to handle this, UPCOMING SOON)
    Also added message conditions for channel commands that notifies the user if an inputted channel ID is invalid or not
    functions.lua - Preliminary SQL dependency, Removed obsoleted functions (that were commented out)
    
    * overhaul workbranch rework
    
    * idk
    
    * undoing screwup
    
    * Overhaul Work - Part 2
    All of this is unfinished, I'm committing so that my work can be synced up to GitHub.
    botmain.lua - Minor changes
    commands.lua - Preparing to update format change
    functions.lua - TO-DO comments
    README.lua - Added new descriptions n' stuff
    
    * Overhaul Work - Part 3
    The bot should run on Heroku normally, but since commands.lua hasn't been formatted yet, command execution won't work.
    botmain.lua - Updated comments, Made some minor adjustments to the startup function, and finally Updated the command parser to use the colon ":" (For native colon support)
    commands.lua - Removed the obsoleted ;deport and ;release commands, and Updated some To-Do comments
    functions.lua - Mainly just updated To-Do comments
    
    * mainToOverhaul + cvRate solution (#6)
    
    ------
    
    * mainToOverhaul + cvRate solution
    I switched over all the coalrate revelcies and have made cvRate into a proper fraction in simplest terms. functions for balance and balance command has been transitioned, feel free to tweek any internal formatting
    
    * minecoal preliminary reply fix
    
    * Update commands.lua
    
    * Create commands.lua
    
    * Update commands.lua
    
    * Suggestions
    Format nitpicking, adjusted the reply message for ;minecoal deny
    
    * Overhaul Work - Part 4
    Some final adjustments..
    
    * Formatting suggestions
    Could this be it?

--- functions.lua
+++ functions.lua
@@ -1,12 +1,20 @@
--- Functions used by the other .lua files
+-- Initialization of functions and variables used by the other .lua files. --
+
+-- local SQL = require("./deps/protgres/postgresLuvit.lua")
+-- TODO: Move all variables from botmain into here (Might consider renaming this file due to this)
+-- Rename functions.lua --> botinit.lua
+-- Rename commands.lua --> botcmds.lua
+-- TODO: Add function IF channel-locked command is ran in an invalid channel, mark it with a reaction
+
 return function(ENV)
-	setfenv(1, ENV)
+	setfenv(1, ENV) -- Connects the main environment from botmain.lua into this file.
 	return {
 		commands = require("./commands.lua")(ENV); -- Loads in the commands into the table so that it can get loaded into the main environment later.
 
 		sleep = function(n) -- In seconds
 			local t0 = os.clock()
 			while os.clock() - t0 <= n do end
+			return true -- For loops
 		end;
 
 		isAdmin = function(userId)
@@ -60,80 +68,6 @@ return function(ENV)
 		clearCoal = function(userId, amount)
 			userMinedCoal[userId] = 0;
 		end;
+    
 	};
 end;
-
---[[ -- Below are the reminants from the old bot war. --
-
-	protectedChannel = client:getChannel("674379623496286243")
-	client:on("channelUpdate", function(upd_channel)
-		if upd_channel == protectedChannel then
-			local perpetrator
-			local logs
-			while true do
-				local _, success = pcall(function()
-					logs = protectedChannel.guild:getAuditLogs({limit=1, type=11})
-					return logs
-				end)
-				print(success)
-				if success then break end
-				sleep(3)
-			end
-			for _,v in pairs(logs) do
-				if v.changes.nsfw and v.changes.nsfw.new == true then
-					perpetrator = v[4]
-					while true do
-						local _, success = pcall(function()
-							--return protectedChannel:disableNSFW()
-						end)
-						print(success)
-						if success then break end
-						sleep(2)
-					end
-					while true do
-						local _, success = pcall(function()
-							--return protectedChannel:send{content="**>:( who the hell tried to enable nsfw? <@"..perpetrator..">? WTF ARE YOU TRYING TO DO?!?!?!**"--[[,tts=true]-]}
-						end)
-						print(success)
-						if success then break end
-						sleep(2)
-					end
-					while true do
-						local _, success = pcall(function()
-							local channel = client:getChannel("569759821713375232")
-							return channel:send{content=">:( <@"..perpetrator.."> enabled NSFW at the mansion.. We're on the way to hell mates."}
-						end)
-						print(success)
-						if success then break end
-						sleep(2)
-					end
-					break
-				end
-			end
-		end
-	end)
-
-
-	if message.channel == protectedChannel and protectedChannel.nsfw == true and (message.attachment ~= nil or message.embeds ~= nil) then
-		--message:reply{content="```ANTI-SERVER DESTRUCTION PROTOCOL INTIATED. DUE TO THE FAILURE TO KEEP THIS PROTECTED CHANNEL SFW, ALL EMBEDDED MESSAGES WILL BE DELETED ON SIGHT.\nNOTE THAT THIS BOT ALSO HAS DIPLOMATIC SANCTIONS. ANY ATTEMPTS TO SABOTAGE THIS AUTOMATED BOT IN ANY WAY IS SUBJECT TO SEVERE CONSEQUENCES.```",tts=true}
-		while true do
-			local _, success = pcall(function()
-				return message:delete()
-			end)
-			print(success)
-			if success then break end
-			sleep(2)
-		end
-		while true do
-			local _, success = pcall(function()
-				--return protectedChannel:disableNSFW()
-			end)
-			print(success)
-			if success then break end
-			sleep(2)
-		end
-		sleep(1)
-		--message:reply("```PROTECTED CHANNEL HAS BEEN PRESERVED. RELIEVING PROTOCOL TEMPORARILY.```")
-	end
-
---]] -- Above are the reminants from the old bot war. --
