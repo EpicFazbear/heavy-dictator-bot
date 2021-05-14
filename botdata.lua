830535abd395c72dd19d9045129904fecca4cd4f
EpicFazbear <EpicFazbear@users.noreply.github.com>
2021-05-14 12:32:06 -0500

    Overhaul v2 Work - Part 1
    This is a work in progress. The most notable change is that botcmds.lua has finally been re-formatted, meaning command execution should work normally now.
    I haven't gone through configuring the new commands format yet, therefore ;help won't appear to run correctly.
    botcmds.lua - Re-formatted the commands table, and created the ;help initializer, also did some minor changes
    botdata.lua - Created botdata.lua (Will be worked on later)
    botinit.lua - Removed the semicolons since we were no longer initializing variables directly inside of the table, Also added the data require (Will be used later on in development)
    botmain.lua - Fixed a typo in the command parser

---
+++ botdata.lua
@@ -0,0 +1,8 @@
+-- Initializes all database-related functions and variables. --
+
+return function(ENV)
+	setfenv(1, ENV) -- Connects the main environment from botmain.lua into this file.
+	local self = {} -- init_table
+
+	return self
+end;
