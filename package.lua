0cb8072fb9e80d13aa963e95f6623cdf7e4bea20
EpicFazbear <EpicFazbear@users.noreply.github.com>
2021-05-15 19:32:48 -0500

    Update package.lua
    Added more detail to package.lua

--- package.lua
+++ package.lua
@@ -3,10 +3,11 @@ return {
   version = "1.2.2",
   description = "A lua-based Discord bot.",
   main = "botmain.lua",
-  scripts = {
-    start = "botmain.lua"
-  },
+  homepage = "https://github.com/EpicFazbear/heavy-dictator-bot",
+	tags = {"discord-bot", "luvit"},
+	license = "MIT",
+	author = "EpicFazbear",
   dependencies = {
     "SinisterRectus/discordia"
-  },
+  }
 }
