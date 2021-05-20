ad492602efcecb7f893f30639c3677660602100c
EpicFazbear <EpicFazbear@users.noreply.github.com>
2021-05-19 20:41:17 -0500

    Push local commits to GItHub
    At the time of writing, data saving in sort of functional. There's still a bunch of bugs to fix, however.
    botcmds.lua - Added a debug command, and added double-checks on the owner commands (making sure the person who ran the command actually has permission to do so)
    botdata.lua - Moved out debug lines, Started work on the data chunk system (Discord only allows polling of the last 100 messages in a channel, so we have to split our data with pinned indicators every 100th message), and added the data:Delete() function
    botinit.lua - Added TODO comment lolz
    botmain.lua - Made some minor rearrangements

--- botdata.lua
+++ botdata.lua
@@ -42,16 +42,17 @@ return function(ENV)
 		self.Synced = false -- Whether or not our Cache is ready yet.
 		self.Cache = {} -- Our local table of data for the bot to quickly reference from.
 		self.Metadata = {} -- A table of user/server IDs linked to their corresponding message IDs in the persistent data channel.
-
-		-- Testing --
-		local dat = self:Save("843352680051245066",{balance=9001},"userdata")
+		self.Pins = nil -- A table of pins (initialized later) in our data_storage channel (our data is processed in 100-msg chunks).
 	end
 
 	function data_table:Sync()
+		if not self.Active then return end
+		print("Initializing database sync.. (Retrieving data from the database)")
 		local pin_pool = data_storage:getPinnedMessages()
 		if #pin_pool == 0 then
 			mark_pin(data_storage)
 		else
+			self.Pins = pin_pool
 			for _, pin in pairs(pin_pool) do
 				local msg_pool = data_storage:getMessagesAfter(pin.id, 100)
 				for _, msg in pairs(msg_pool) do
@@ -105,15 +106,37 @@ return function(ENV)
 		if datatype == nil then datatype = data.type end
 		data = self:Serialize(data, datatype)
 		data.id = id
+		self.Cache[id] = data
 		local encoded = self:Encode(data, datatype)--:gsub(",", ",\n	"):gsub("{","{\n	"):gsub("}","\n}")
-		local message = data_storage:getMessage(id)
-		if message ~= nil and message.author.id == client.user.id then
-			message:setContent("```json\n".. encoded .."\n```") -- Since data already exists for this ID, simply overwrite it
+		local message = self.Metadata[id]
+		if message ~= nil then
+			message = data_storage:getMessage(message)
+			if message ~= nil and message.author.id == client.user.id then
+				message:setContent("```json\n".. encoded .."\n```") -- Since data already exists for this ID, simply overwrite it
+			end
 		else
-			data_storage:send{content = encoded, code = "json"} -- Create new data for our unique ID
+			message = data_storage:send{content = encoded, code = "json"} -- Create new data for our unique ID
+			self.Metadata[id] = message.id
 			-- TODO: Add detection for when we've reached 100 messages since the last PIN
+			print("Checking if we have reached the data chunk limit..")
+			local debug_time = os.time()
+			local check = false
+			local msg_pool = data_storage:getMessages(100)
+			for _, msg  in pairs(msg_pool) do
+				for _, pin  in pairs(msg_pool) do
+					if msg.id == pin.id then
+						check = true
+					end
+				end
+			end
+			if check == true then
+				print("Chunk limit reached. Creating new chunk separator..", os.time() - debug_time)
+				mark_pin(data_storage)
+			else
+				print("Chunk limit not reached yet..", tick() - debug_time)
+			end
 		end
-		return encoded
+		return self.Cache[id]
 	end
 
 	function data_table:Modify(id, key, value)
@@ -122,6 +145,18 @@ return function(ENV)
 		--assert(type(value) == "string", "No data VALUE was provided!")
 		self.Cache[id][key] = value
 		self:Save(id, self.Cache[id], self.Cache[id].type)
+	end
+
+	function data_table:Delete(id)
+		assert(type(id) == "string", "An invalid ID was provided!")
+		local message = self.Metadata[id]
+		if message ~= nil then
+			message = data_storage:getMessage(message)
+			if message ~= nil then
+				message:delete() -- This is irreversible.
+				self.Cache[id] = nil
+			end
+		end
 	end
 
 	return data_table
