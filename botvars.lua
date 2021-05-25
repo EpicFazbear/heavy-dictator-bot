c8b9ba0f12a2b3f79740a3f9aab2f6659ce5618a
EpicFazbear <EpicFazbear@users.noreply.github.com>
2021-05-25 09:53:47 -0500

    Overhaul v2 Work - Part 4
    A lot of changes have been made since the last part was published. Data saving should be bug-free, but actual implementation has yet to be completed. Here's a list of the other changes made:
    botcmds.lua - Renamed ;dataedit to ;datamod.
    botdata.lua - Updated some variables, Fixed many of the bugs concerning data saving and loading into Cache/Metadata, Added print errors (letting you know what went wrong), and made some other minor adjustments.
    botinit.lua - Added a getPRC (botvars.lua) function that allows loading of our .env variables even if we didn't use `heroku local`.
    botmain.lua - Updated some variable names, and integrated botvars.lua (Allowing the bot to run solely on luvit).
    botvars.lua - Created the botvars.lua file, which can read and parse our variables from .env!

---
+++ botvars.lua
@@ -0,0 +1,28 @@
+-- Manually processes our .env file if the bot isn't ran through the Heroku CLI. --
+
+-- RegEx is scary
+local function parse(input)
+  local s1, s2 = string.match(input, "(.*)=%s*(.*)\r")
+  if s1 == nil or s2 == nil then return false end
+  s1 = string.sub(s1, 1, (string.find(s1, "%s+$") or #s1+1)-1)
+  return s1, s2
+end
+
+-- http://lua-users.org/wiki/FileInputOutput
+local file_name = ".env"
+local VARS = {}
+local file = io.open(file_name, "rb")
+if file ~= nil then
+  file:close()
+  for line in io.lines(file_name) do
+    local key, value = parse(line)
+    if key ~= false then
+      VARS[key] = value
+    end
+  end
+end
+
+-- Returns the corresponding value to our variable name
+return function(env_name)
+  return VARS[env_name]
+end
