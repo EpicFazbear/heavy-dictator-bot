777437e6ed22a0597242bc4c81095320e1c3c1f8
EpicFazbear <EpicFazbear@users.noreply.github.com>
2021-05-26 00:31:08 -0500

    Update botvars.lua
    Converted spaces into tabs

--- botvars.lua
+++ botvars.lua
@@ -2,10 +2,10 @@
 
 -- RegEx is scary
 local function parse(input)
-  local s1, s2 = string.match(input, "(.*)=%s*(.*)\r")
-  if s1 == nil or s2 == nil then return false end
-  s1 = string.sub(s1, 1, (string.find(s1, "%s+$") or #s1+1)-1)
-  return s1, s2
+	local s1, s2 = string.match(input, "(.*)=%s*(.*)\r")
+	if s1 == nil or s2 == nil then return false end
+	s1 = string.sub(s1, 1, (string.find(s1, "%s+$") or #s1+1)-1)
+	return s1, s2
 end
 
 -- http://lua-users.org/wiki/FileInputOutput
@@ -13,16 +13,16 @@ local file_name = ".env"
 local VARS = {}
 local file = io.open(file_name, "rb")
 if file ~= nil then
-  file:close()
-  for line in io.lines(file_name) do
-    local key, value = parse(line)
-    if key ~= false then
-      VARS[key] = value
-    end
-  end
+	file:close()
+	for line in io.lines(file_name) do
+		local key, value = parse(line)
+		if key ~= false then
+			VARS[key] = value
+		end
+	end
 end
 
 -- Returns the corresponding value to our variable name
 return function(env_name)
-  return VARS[env_name]
+	return VARS[env_name]
 end
